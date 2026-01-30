32674e0049de965478def137fffc9d1d0fe9ede7
Chief-Zach <59001347+Chief-Zach@users.noreply.github.com>
2026-01-30 14:18:43 -0500

    Better sample level for user testing

--- application/levels/sample_level.py
+++ application/levels/sample_level.py
@@ -1,65 +1,68 @@
-from random import sample
-
 from fastapi import Request
 from fastapi.responses import HTMLResponse
-from starlette.responses import Response
+from starlette.responses import Response, JSONResponse
 from json import JSONDecodeError
-from app.utils.extensions import generate_button, templates, Table
+from app.utils.extensions import generate_button, templates, FormGroup, FormData, generate_hidden_text
 from app.utils.page import Page
 import json
 from config import get_config
+from typing import Annotated, Optional
+from fastapi import Form
 
 config = get_config()
 
-sample_game = Page("Sample Level") # sample_level
-game = sample_game.route
 
-sample_game.load_scripts({"password_form": "password_form.js"})
-
-async def instructions(request: Request):
-    text = "This is your instruction text"
-    button = generate_button(text="Frontend", link=f"{config.URL}{sample_game.url_prefix}/frontend")
-
-    response = templates.TemplateResponse(request=request, name=config.TEMPLATE,
-                                          context=
-                                          {
-                                              "header": sample_game.name,
-                                              "text": text,
-                                              "primary_button": button
-                                          })
-
-    return response
+password_level = Page("Password Problems")
+password_level.load_scripts({"password_form": "password_form.js"})
+game = password_level.route
 
 async def verify(request: Request):
     try:
         data = await request.json()
         user_cookie = request.cookies.get("user")
-        if sample_game.parameterization.parameterize_flag(user_cookie, sample_game.level_code) == data.get("flag", None):
+        if password_level.parameterization.parameterize_flag(user_cookie, password_level.level_code) == data.get("flag", None):
             return {"success": True}
-
         else:
-            return {"success": False}
-    except:
-        return {"success": False}
+            return {"success": False, "error": {"code": 403, "text": "Unauthorized"}}
 
-sample_game.set_functions(instructions, verify)
+    except (JSONDecodeError, KeyError):
+        return {"success": False, "error": {"code": 403, "text": "Unauthorized"}}
+
+async def instructions(request: Request):
+    text = "Your username is admin and your password is password."
+    button = generate_button("Frontend", f"{config.URL}{password_level.url_prefix}/frontend")
+
+    response = templates.TemplateResponse(request=request, name=config.TEMPLATE, context={"header": password_level.name,
+                                    "text": text, "primary_button": button})
+
+    return response
+
+password_level.set_functions(verify=verify, instructions=instructions)
+
+@game.post("/login", response_class=JSONResponse)
+def login(request: Request, username: Annotated[Optional[str], Form()] = None,
+          password: Annotated[Optional[str], Form()] = None):
+    user_cookie = request.cookies.get("user", None)
+
+    if username == "admin" and password == "password":
+        return JSONResponse(f"Login Success "
+                            f"{password_level.parameterization.parameterize_flag(user_cookie, password_level.level_code)}",
+                            status_code=200)
+    return JSONResponse("Incorrect Password", status_code=403)
+
 
 @game.get("/frontend", response_class=HTMLResponse)
 async def frontend(request: Request):
-    user_cookie = request.cookies.get("user")
+    user_cookie = request.cookies.get("user", None)
 
-    flag = sample_game.parameterization.parameterize_flag(user_cookie, sample_game.level_code)
+    if user_cookie is None:
+        return Response(500)
 
-    part_size = len(flag) // 5
+    form_groups = [FormGroup("username", "text", "usernameInput", "Username"),
+        FormGroup("password", "password", "passwordInput", "Password")]
 
-    parts = [flag[i:i + part_size] for i in range(0, len(flag), part_size)]
+    form_data = FormData(f"{config.URL}{password_level.url_prefix}/login", form_groups).generate_form()
 
-    table = Table([{f"Part {number + 1}": part for number, part in enumerate(parts)}])
-
-
-    return templates.TemplateResponse(request=request, name=config.TEMPLATE,
-                                      context={
-                                          "header": sample_game.name,
-                                          "table": table.get_html()
-                                      })
-
+    return templates.TemplateResponse(request=request, name=config.TEMPLATE, context={"header": password_level.name
+                                      ,"form_data": form_data,
+                                      "scripts": password_level.scripts["password_form"]})
--- application/my_pages.py
+++ application/my_pages.py
@@ -1,7 +1,7 @@
 from app.utils.game_setup import Pages_Object
 # Import all of your games here. Be sure to import the "game_class" or whatever you choose to name it
 from levels.welcome_game import welcome_game
-from levels.sample_level import sample_game
+from levels.sample_level import password_level
 # from .levels.my_next_game import my_next_game
 
 def my_pages():
@@ -12,7 +12,7 @@ def my_pages():
     # Put all of your "non-games" at the front to ensure no cross over.
     # Register the rest of your games in the order you want users to play them
     my_games_list.append(welcome_game)
-    my_games_list.append(sample_game)
+    my_games_list.append(password_level)
     # my_games_list.append(my_next_game)
     # Leave this here
     return my_games_list
