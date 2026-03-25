9f2aa889acc819fdd5adbce5f93b8912f72dcbbe
Chief-Zach <59001347+Chief-Zach@users.noreply.github.com>
2026-03-25 12:05:36 -0400

    Forgot hash docstring

--- application/app/utils/aes_service.py
+++ application/app/utils/aes_service.py
@@ -12,6 +12,12 @@ class SHA256Service:
     def __init__(self):
         self.hashing = SHA256.new()
     def hash(self, data: str) -> SHA256Hash:
+        """
+        :arg
+        data: String data to hash
+        :return:
+        Bytes of hashed data. Must be hex digested to turn into a string
+        """
         return self.hashing.new(data.encode())
 
 hashing_service = SHA256Service()
